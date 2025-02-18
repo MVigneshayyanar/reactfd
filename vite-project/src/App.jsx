cfecda91c97395fa5b970d2ee18f693ff9d99938
faKe <vicky69tn@gmail.com>
2025-02-18 23:17:24 +0530

    fifth

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import './App.css'
 import Component from './Component';
 function App() {
+
+  const [mode,setMode]=useState(true)
+
+  const switchMode=()=>{
+    setMode(!mode)
+  }
 
   const person1={
     name:"Vignesh INR",
@@ -16,15 +23,16 @@ function App() {
   const{age:hbd}=person1
   console.log("Happy ",hbd," guys")
   return(
-    <>
+    <div id="main" className={mode?"lm":"dm"}>
     <div id="container2">
     <Component firstName={person1.name} firstAge={person1.age} firstCity={person1.city} secondName={person2.name} secondAge={person2.age} secondCity={person2.city}/>
      </div>
+     <button onClick={switchMode}>Switch Mode</button>
     <div id="container1">
     <button onClick={()=>alert("Thanks for the Click")}>SINGLE CLICK</button>
     <button onDoubleClick={()=>alert("Thanks for the Double Click")}>DOUBLE CLICK</button>
     </div>
-    </>
+    </div>
   )
 }
 
