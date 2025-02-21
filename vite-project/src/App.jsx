0490b7c9325738cf10d83cf04fa2c71712fc45c2
faKe <vicky69tn@gmail.com>
2025-02-21 22:54:32 +0530

    sixth form

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
-import Person from './Component';
 
 function App() {
 
-  const person1 = {
-    name: 'faKe',
-    age: 19,
-    city: 'Chennai',
-    gender: 'M',
-  };
-  const person2 = {
-    name: 'fake',
-    age: 19,
-    city: 'Tvl',
-    gender:   'F',
-  };
-
-  console.log(`Happy ${person1.age} guys`);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Details are updated successfully.`);
+  }
 
   return (
   <>
-    <Person name={person1.name} age={person1.age} city={person1.city} gender={person1.gender}/>
-    <Person name={person1.name} age={person1.age} city={person1.city} gender={person1.gender}/>
+    <form onSubmit={handleSubmit} >
+      <input type="text" id="name" placeholder='Enter your name' required value={name} onChange={(e)=>setName(e.target.value)}/>
+      <input type="email" name="" id="email" placeholder='Enter you email' required value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      <input type="password" name="" id="pass" placeholder='Enter your password' required value={pass} onChange={(e)=>setPass(e.target.value)}/>
+      <button type="submit" value="Submit">Submit</button>
+    </form>
   </>
     
   );
